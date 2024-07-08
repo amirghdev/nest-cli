7150dc423d8529a860445e99559f54217d32a950
amirghdev <amirkodeta99@gmail.com>
2024-07-08 21:38:34 +0330

    :wrench: update folders structure

---
+++ commands.js
@@ -0,0 +1,70 @@
+require("dotenv").config();
+//* imports
+const { handleGit, handleEnv } = require("./actions");
+
+//* modules
+const inquirer = require("inquirer");
+const ui = new inquirer.ui.BottomBar();
+
+module.exports.addCommand = async () => {
+  if (!process.env.GIT_USER) {
+    const data = await inquirer.prompt([
+      {
+        type: "question",
+        name: "username",
+        message: "enter your git username",
+      },
+    ]);
+    await handleEnv("GIT_USER", data.username);
+  }
+  if (!process.env.GIT_PASSWORD) {
+    const data = await inquirer.prompt([
+      {
+        type: "password",
+        name: "password",
+        message: "enter your git password (token)",
+      },
+    ]);
+    await handleEnv("GIT_PASSWORD", data.password);
+  }
+  if (!process.env.GIT_REPOSITORY) {
+    const data = await inquirer.prompt([
+      {
+        type: "question",
+        name: "repository",
+        message: "enter your git repo with .git",
+      },
+    ]);
+    await handleEnv("GIT_REPOSITORY", data.repository);
+  }
+  let folders = "";
+  if (!process.env.GIT_FOLDERS) {
+    console.log("there is no GIT_FOLDERS file");
+    const data = await inquirer.prompt([
+      {
+        type: "question",
+        name: "folders",
+        message: "enter your git modules like this (test,test1,test2,test3)",
+      },
+    ]);
+    folders = data.folders;
+    await handleEnv("GIT_FOLDERS", data.folders);
+  }
+
+  const choices = process.env.GIT_FOLDERS;
+  const list = choices.split(",");
+  const action = await inquirer.prompt([
+    {
+      type: "list",
+      name: "module",
+      message: "select the module name you want to clone:",
+      choices: list,
+    },
+  ]);
+  await handleGit(action.module);
+};
+
+module.exports.helpCommand = async () => {
+  ui.log.write("hi welcome to my nestjs basic cli use add command");
+  process.exit(0);
+};
--- index.js
+++ index.js
@@ -2,85 +2,19 @@
 require("dotenv").config();
 const { program } = require("commander");
 
-const inquirer = require("inquirer");
-
-const { handleDatabase, handleEmail, handleHelper, handleUpload, handleEnv, handleGit } = require("./action");
-const ui = new inquirer.ui.BottomBar();
+//* commands
+const { addCommand, helpCommand } = require("./commands");
 
 try {
   program.version("1.0.0").description("Custom CLI for managing NestJS project");
 
-  program.command("add").action(async () => {
-    if (!process.env.GIT_USER) {
-      const data = await inquirer.prompt([
-        {
-          type: "question",
-          name: "username",
-          message: "enter your git username",
-        },
-      ]);
-      handleEnv("GIT_USER", data.username);
-    }
-    if (!process.env.GIT_PASSWORD) {
-      const data = await inquirer.prompt([
-        {
-          type: "password",
-          name: "password",
-          message: "enter your git password (token)",
-        },
-      ]);
-      handleEnv("GIT_PASSWORD", data.password);
-    }
-    if (!process.env.GIT_REPOSITORY) {
-      const data = await inquirer.prompt([
-        {
-          type: "question",
-          name: "repository",
-          message: "enter your git repo with .git",
-        },
-      ]);
-      handleEnv("GIT_REPOSITORY", data.repository);
-    }
-    let folders = "";
-    if (!process.env.GIT_FOLDERS) {
-      console.log("there is no GIT_FOLDERS file");
-      const data = await inquirer.prompt([
-        {
-          type: "question",
-          name: "folders",
-          message: "enter your git modules like this (test,test1,test2,test3)",
-        },
-      ]);
-      folders = data.folders;
-      handleEnv("GIT_FOLDERS", data.folders);
-    }
-    addCommand();
-  });
-  program.command("help").action(async () => {
-    ui.log.write("hi welcome to my nestjs basic cli use add command");
-    process.exit(0);
-  });
+  //? command lists
+  program.command("add").action(addCommand);
+  program.command("help").action(helpCommand);
+
   program.parse();
 } catch (error) {
   console.log("error");
   console.log(error);
   return error;
-}
-
-async function addCommand() {
-  try {
-    const choices = process.env.GIT_FOLDERS;
-    const list = choices.split(",");
-    const action = await inquirer.prompt([
-      {
-        type: "list",
-        name: "module",
-        message: "select the module name you want to clone:",
-        choices: list,
-      },
-    ]);
-    await handleGit(action.module);
-  } catch (error) {
-    console.log(error);
-  }
 }
